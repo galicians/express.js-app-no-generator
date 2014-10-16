ca33a195cbfcad1f9a25e2e146c867daffd941e3
galicians <galicians@gmail.com>
2014-10-16 19:45:48 +0100

    routes all, server instanciated, jade added

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade')
 
+app.all('*', function(req, res) {
+	res.render(
+		'index', {msg: 'Welcome to Pablo s Node.js app'}
+		);
+});
+
+http
+	.createServer(app)
+	.listen(
+		app.get('port'),
+		function() {
+			console.log('Express.js server listening on port' + app.get('port')
+				);
+			}
+		);
