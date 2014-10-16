1ea61d1c74efec4a3a71a7f0c8a27fb3a304527e
galicians <galicians@gmail.com>
2014-10-17 00:16:29 +0100

    using chai module with mocha

--- test-example/test.js
+++ test-example/test.js
@@ -1,7 +1,26 @@
 
-var assert = require('assert');
+var assert = require('chai').assert;
+
 describe('String#split', function() {
+	
+	before(function() {
+		expected = ['a','b','c'];
+	})
+	
+	beforeEach(function() {
+		current = 'a,b,c'.split(',');
+	})
+	
+
 	it('should return an array', function() {
-		assert(Array.isArray('a,b,c'.split(',')));
+		assert(Array.isArray(current));
 	});
+
+	it('should return the same array', function() {
+		assert.equal(['a','b','c'].length, 'a,b,c'.split(',').length, 'arrays have equal length');
+		for(var i=0; i<expected.length; i++) {
+			assert.equal(expected[1],  current[1], i + 'element is equal');
+		};
+	});
+
 })
